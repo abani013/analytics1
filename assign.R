afbb0baebeab53f54e10daaae4ad85713d537314
abani013 <abanindrad@gmail.com>
2018-10-25 14:16:22 +0530

    my first code

---
+++ assign.R
@@ -0,0 +1,54 @@
+# Starting to work on R
+# assign ----
+x1 = 3
+x2 <- 3
+#print values
+x1
+x2
+# Assign and print
+(x3=3)
+# environment
+# variables in env
+ls()
+# datasets available for use
+data()
+?women
+mtcars
+attach(mtcars)
+mgp
+# libraries currently loaded
+library()
+#Elements
+#ls()
+rm(list = ls())
+rm(list = ls(all = TRUE))
+#help
+
+?mean
+x <- c(0:10, 50)
+x
+print(x)
+xm <- mean(x)
+xm
+c(xm, mean(x, trim = 0.10))
+
+?mode
+
+# create values
+22:100
+
+seq(0,200, by=2)
+seq(1,200, by=2)
+
+seq(1,10, length.out = 5)
+x=c(0:10)
+
+rep(c(1 ,10), times=10)
+rep(c(1 ,10), each=10)
+rep(c(1,4), times=c(2,6))
+
+Sys.Date()
+
+
+
+
